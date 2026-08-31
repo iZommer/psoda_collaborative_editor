5f64a71c866cb121887d1eed74d4afc4381ff236
Jamie <williamsjamie24@gmail.com>
2026-08-31 22:03:32 +1200

    update to fix vercel connectio to db

--- JW_webapp/api/index.js
+++ JW_webapp/api/index.js
@@ -16,17 +16,17 @@ const app = express()
 app.use(express.json())
 app.use(express.static(path.join(process.cwd(), 'public')))
 
-app.get('/', (req, res) => {
+app.get(['/api', '/api/'], (req, res) => {
     res.sendFile(path.join(process.cwd(), 'public', 'index.html'))
 })
 
-app.post('/app/users', handleCreateUser)
-app.get('/app/document', handleGetDocument)
-app.put('/app/document', handleUpdateDocument)
-app.post('/app/document', handleCreateDocument)
-app.get('/app/documents', handleGetDocuments)
-app.delete('/app/document', handleDeleteDocument)
-app.post('/app/cursor', handleUpdateCursor)
-app.get('/app/sync', handleSync)
+app.post('/api/app/users', handleCreateUser)
+app.get('/api/app/document', handleGetDocument)
+app.put('/api/app/document', handleUpdateDocument)
+app.post('/api/app/document', handleCreateDocument)
+app.get('/api/app/documents', handleGetDocuments)
+app.delete('/api/app/document', handleDeleteDocument)
+app.post('/api/app/cursor', handleUpdateCursor)
+app.get('/api/app/sync', handleSync)
 
 export default app
